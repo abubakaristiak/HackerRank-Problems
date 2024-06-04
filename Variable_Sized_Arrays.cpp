cc9d2641c7f36f99e281e57a389b67dd608944db
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-06-04 09:48:19 +0600

    add new program

--- Variable_Sized_Arrays.cpp
+++ Variable_Sized_Arrays.cpp
@@ -1,27 +1,48 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+   
+//     int row = 0, col = 0, q = 0;
+//     cin >> row >> q;
+
+//     int* arr[row];
+
+//     for(int i=0; i<row; ++i){
+
+//         cin >> col;
+//         arr[i] = new int[col];
+
+//         for(int j=0; j<col; ++j) cin >> arr[i][j];
+
+//     }
+
+//     for(int i=0; i<q; ++i){
+//         cin >> row >> col;
+//         cout << arr[row][col] << endl;
+//     }
+
+//     return 0;
+// }
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    int row = 0, col = 0, q = 0;
-    cin >> row >> q;
+    int row,col,q;
+    cin >> row>> q;
 
-    int* arr[row];
-
-    for(int i=0; i<row; ++i){
-
-        cin >> col;
-        arr[i] = new int[col];
-
-        for(int j=0; j<col; ++j) cin >> arr[i][j];
+    int *ar[row];
+    for(int i=0; i<row; i++){
+        cin >>col;
+        ar[i] = new int[col];
+        for(int j=0; j<col; j++){
+            cin >> ar[i][j];
+        } 
 
     }
-
-    for(int i=0; i<q; ++i){
+    for(int i=0; i<q; i++){
         cin >> row >> col;
-        cout << arr[row][col] << endl;
+        cout << ar[row][col]<<endl;;
     }
-
     return 0;
 }
-
