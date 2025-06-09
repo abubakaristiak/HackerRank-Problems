dc36af710096cc2bba6531064c01d56285ad5db0
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-06-09 16:07:53 +0600

    updated this problem

--- Problem Solving/Bigger_is_Greater.cpp
+++ Problem Solving/Bigger_is_Greater.cpp
@@ -76,3 +76,27 @@ int main() {
 
     // Alhamdulillah
 }
+
+
+
+/*
+another approach:
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int test;
+    cin >> test;
+    while(test--) {
+        string inp;
+        cin >> inp;
+        if (next_permutation(inp.begin(), inp.end()) == false ) {
+            cout << "no answer" << endl;
+        }
+        else {
+            cout << inp << endl;
+        }
+    }
+    return 0;
+}
+*/
